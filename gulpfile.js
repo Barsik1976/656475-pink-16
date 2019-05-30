0004c530447655e0d39f7be650f9c2bc49cd4cec
unknown <C:\Users\Sasha\AppData\Roaming\The Bat!>
2019-05-30 15:40:49 +0300

    Верстка таблицы и полифинилы

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var gulp = require("gulp");
-var del = require("del");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
@@ -9,75 +8,30 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
-gulp.task("copy", function () {
-return gulp.src([
-"source/fonts/**/*.{woff,woff2}",
-"source/img/**",
-"source/js/**",
-"source/*.ico"
-], {
-base: "source"
-})
-.pipe(gulp.dest("build"));
-});
-
-gulp.task("clean", function () {
-return del("build");
-});
-
 gulp.task("css", function () {
-return gulp.src("source/less/style.less")
-.pipe(plumber())
-.pipe(sourcemap.init())
-.pipe(less())
-.pipe(postcss([
-autoprefixer()
-]))
-.pipe(csso())
-.pipe(rename("style.min.css"))
-.pipe(sourcemap.write("."))
-.pipe(gulp.dest("build/css"));
-.pipe(server.stream());
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
-
-gulp.task("symbols", function () {
-return gulp.src("source/img/icon-*.svg")
-.pipe(svgstore({
-inlineSvg: true
-}))
-.pipe(rename("symbols.svg"))
-.pipe(gulp.dest("build/img"));
-});
-
-gulp.task("html", function () {
-return gulp.src("source/*.html")
-.pipe(posthtml([
-include()
-]))
-.pipe(gulp.dest("build"));
-});
-
-gulp.task("build", gulp.series(
-"clean",
-"copy",
-"css",
-"symbols",
-"html"
-));
 
 gulp.task("server", function () {
-server.init({
-server: "build/"
-});
-
-gulp.task("refresh", function (done) {
-server.reload();
-done();
-});
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("symbols", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("start", gulp.series("css", "server"));
